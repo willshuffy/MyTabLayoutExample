3f658c4399f79508dee57e31632b8d95f19c8d8c
willshuffy <saputrawilly26@gmail.com>
2020-10-26 08:04:46 +0700

    Setup ViewPager and TabLayout

--- app/src/main/java/com/willshuffyproject/mytablayoutexample/MainActivity.kt
+++ app/src/main/java/com/willshuffyproject/mytablayoutexample/MainActivity.kt
@@ -2,11 +2,18 @@ package com.willshuffyproject.mytablayoutexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        view_pager.adapter = sectionsPagerAdapter
+        tabs.setupWithViewPager(view_pager)
+
+        supportActionBar?.elevation = 0f
     }
 }
